eb0ec71aa1f0e5ea1514b9858aece968d3951474
Ivan Tyurin <me@ivantyurin.ru>
2020-04-01 13:36:40 +0300

    Storyboard

---
+++ Learning Swift Part 2/Learning Swift Part 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learning Swift Part 2
+//
+//  Created by Ivan Tyurin on 01.04.2020.
+//  Copyright © 2020 Ivan Tyurin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
