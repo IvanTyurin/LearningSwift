97b0767f97980e24e8da3d199fc81781a82bc772
Ivan Tyurin <me@ivantyurin.ru>
2020-04-21 15:26:01 +0300

    Start learning ARC
    Создал проект

---
+++ ARCLearning/ARCLearning/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARCLearning
+//
+//  Created by Ivan Tyurin on 21.04.2020.
+//  Copyright © 2020 Ivan Tyurin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
