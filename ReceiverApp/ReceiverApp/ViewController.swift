2c6050e19038eb5077f4b27230c8f21b33255c7d
Ivan Tyurin <ivan.tyurin.job@gmail.com>
2020-05-08 21:06:24 +0300

    URL Scheme
    Done

---
+++ ReceiverApp/ReceiverApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  ReceiverApp
+//
+//  Created by Ivan Tyurin on 08.05.2020.
+//  Copyright © 2020 Ivan Tyurin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var receivedTextLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
